14b2bc6a62e82ec93c39b19f4d16c95048164192
hamzehamiri <solidworkerh@gmail.com>
2020-01-14 11:28:59 +0330

    Spring Init

---
+++ src/main/java/springtutorials/foadandhamzeh/FoadandhamzehApplication.java
@@ -0,0 +1,13 @@
+package springtutorials.foadandhamzeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoadandhamzehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoadandhamzehApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springtutorials/foadandhamzeh/FoadandhamzehApplicationTests.java
@@ -0,0 +1,13 @@
+package springtutorials.foadandhamzeh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoadandhamzehApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
